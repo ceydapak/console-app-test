db1bdd445a754c5821226500bd80d4497a7d563a
Ceyda PAK <ceyda12345pak@gmail.com>
2021-04-18 18:42:49 +0300

    All finish

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace console_app_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList spektrum = new ArrayList();
+            spektrum.Add("Nova");
+            spektrum.Add("Aris");
+            spektrum.Add("Lamina");
+            spektrum.Add("Vidar");
+            spektrum.Add("Ütopya");
+
+            Console.Write("Kullanıcı adınızı giriniz:");
+            string name = Console.ReadLine();
+
+            if (spektrum.Contains(name) == true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Ritminizin rengi spektrum, Hoşgeldiniz {0} !", name);
+                Console.WriteLine();
+            }
+
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("spektrum'dan değilsiniz, girişiniz engellendi {0} ! ", name);
+                Console.WriteLine();
+            }
+        }
+    }
+}
